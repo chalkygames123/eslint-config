2df97a153bde02fb3346c3b0ea8c86ef5047645d
Takuya Fukuju <chalkygames123@gmail.com>
2023-02-13 17:43:08 +0900

    chore(lint-staged): update `package-lock.json` if `package.json` is changed

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -3,4 +3,5 @@
 module.exports = {
 	'*.js': ['eslint --fix', 'prettier --check --write'],
 	'!*.js': 'prettier --check --write --ignore-unknown',
+	'package.json': () => ['npm install', 'git add package-lock.json'],
 };
